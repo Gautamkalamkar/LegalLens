e262495d9f8ec7f846d8b4a1eb7b39d23bc291c4
Gautam <gautamkalamkar4@gmail.com>
2024-07-25 16:57:48 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:animated_splash_screen/animated_splash_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:legallens/pages/home_page.dart';
+import 'package:page_transition/page_transition.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'LegalLens',
+        home: AnimatedSplashScreen(
+          splash: Image.asset('assets/images/logo.png'),
+          nextScreen: HomePage(),
+          duration: 2000,
+          splashTransition: SplashTransition.scaleTransition,
+          centered: true,
+          splashIconSize: 150.0,
+          curve: Curves.fastEaseInToSlowEaseOut,
+          pageTransitionType: PageTransitionType.topToBottom,
+          backgroundColor:
+              (const Color.fromRGBO(240, 244, 195, 1)), //Hex value: #f0f4c3
+        ));
+  }
+}
