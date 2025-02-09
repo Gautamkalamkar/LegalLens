1409e872bf65268c9e21d412171e8af2a16eda00
Gautam <gautamkalamkar4@gmail.com>
2025-02-09 07:08:00 -0800

    Added AppBar to the App

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -11,8 +11,30 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
+    var size = MediaQuery.sizeOf(context);
     return Scaffold(
-      backgroundColor: Theme.of(context).primaryColor,
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        title: Padding(
+          padding: EdgeInsets.only(left: size.width * 0.02),
+          child: Text('LegalLens'),
+        ),
+        titleTextStyle: TextStyle(
+            fontFamily: 'Lexend',
+            color: Theme.of(context).colorScheme.onPrimary,
+            fontSize: size.height * 0.03),
+        actions: [
+          Padding(
+            padding: EdgeInsets.only(right: size.height * 0.02),
+            child: Icon(
+              Icons.more_vert,
+              color: Theme.of(context).colorScheme.onPrimary,
+              size: size.height * 0.03,
+            ),
+          )
+        ],
+      ),
+      backgroundColor: Theme.of(context).colorScheme.primary,
       body: Container(
         padding: EdgeInsets.all(20),
         child: GridView.count(
